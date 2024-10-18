0286942f487bacf105cfb4eb3317026d24b92feb
gigadiaswara <diaswara.giga@gmail.com>
2024-10-18 08:35:03 +0700

    3

--- src/views/heroSection.tsx
+++ src/views/heroSection.tsx
@@ -1,6 +1,6 @@
 const HeroSection = () => {
     return (
-      <section id="about" className="h-screen snap-start flex items-center justify-center bg-gray-100">
+      <section id="about" className="h-screen snap-start flex items-center justify-self-stretch bg-gray-100">
         <h1 className="text-4xl md:text-6xl font-bold">Welcome to Ageka Konstruksi</h1>
       </section>
 
--- src/views/serviceSection.tsx
+++ src/views/serviceSection.tsx
@@ -1,6 +1,6 @@
 const ServicesSection = () => {
     return (
-      <section id="ourServices" className="h-screen flex items-center justify-center bg-white">
+      <section id="ourServices" className="h-screen flex items-center justify-self-stretch bg-white">
         <h2 className="text-3xl font-bold mb-6">Our Services</h2>
         <p className="text-lg max-w-3xl mx-auto">
         At Ageka Konstruksi, we specialize in building repairs and maintenance, ensuring your property remains safe, functional, and well-kept. Whether it’s a small fix or a major repair, our team is dedicated to providing top-notch service to keep your building in the best possible condition. We’re not just about maintenance – we’re also ready to tackle new challenges in construction. Got a unique project or a tough problem? Bring it on! Our team thrives on finding creative solutions for anything related to building construction. Let Ageka Konstruksi be your go-to partner for all things building-related. We're here to make sure your property stays strong, reliable, and ready for whatever comes next.
